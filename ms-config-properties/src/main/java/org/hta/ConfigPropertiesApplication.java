c85ac2a2514782fc047b70aab86e37d5c91a304c
walavo <alvaro@preventor.com>
2021-05-07 21:25:40 -0500

    feat: Config Properties and false ip

--- ms-config-properties/src/main/java/org/hta/ConfigPropertiesApplication.java
+++ ms-config-properties/src/main/java/org/hta/ConfigPropertiesApplication.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
 @EnableConfigServer
-public class PropertiesApplication {
+public class ConfigPropertiesApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PropertiesApplication.class, args);
+		SpringApplication.run(ConfigPropertiesApplication.class, args);
 	}
 
 }
